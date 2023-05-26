a152e73a4f0ec9b935ce784b7e53646e1e0c45db
eliza2598 <elizabeth.bermudezp@ecci.edu.co>
2023-05-26 17:41:14 -0500

    Triangulo números seguidos

---
+++ triaungulonumerosrepetidos.py
@@ -0,0 +1,25 @@
+# numeros
+
+numero = "1"
+pos = 10
+conteo=0
+while conteo <1:
+    print (1, end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    print ("2 2", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    print ("3 3 3", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    print ("4 4 4 4", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
