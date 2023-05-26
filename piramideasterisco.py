c8934c6c93a327a68956f1bd107fc8737060ad2d
eliza2598 <elizabeth.bermudezp@ecci.edu.co>
2023-05-26 17:32:30 -0500

    Piramide en asteriscos

---
+++ piramideasterisco.py
@@ -0,0 +1,28 @@
+# programa piramide
+
+numero = "*"
+pos = 10
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("*", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <2:
+    #print(conteo)
+    print ("*", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <3:
+    #print(conteo)
+    print ("*", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <4:
+    #print(conteo)
+    print ("*", end =" ")
+    conteo = conteo +1
+print(" ")
