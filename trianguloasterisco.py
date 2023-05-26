12ac05d45509de140757e79e8fda764a93c80271
eliza2598 <elizabeth.bermudezp@ecci.edu.co>
2023-05-26 17:35:35 -0500

    Triangulo recto en asterisco

---
+++ trianguloasterisco.py
@@ -0,0 +1,27 @@
+numero = "*"
+pos = 10
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("*", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("**", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("*", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("****", end =" ")
+    conteo = conteo +1
+print(" ")
+
