060c5a35466d429ef0bb936a967c678ec2bc7537
eliza2598 <elizabeth.bermudezp@ecci.edu.co>
2023-05-26 17:39:02 -0500

    Triangulo en Números

---
+++ triaungulonumeros.py
@@ -0,0 +1,25 @@
+# numeros
+
+numero = "1"
+pos = 10
+conteo=0
+while conteo <1:
+    print (1, end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    print ("1 2", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    print ("1 2 3", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    print ("1 2 3 4", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
