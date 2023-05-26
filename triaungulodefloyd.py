2f65e02570edf47d333a148dd22d0f85fd1883a7
eliza2598 <elizabeth.bermudezp@ecci.edu.co>
2023-05-26 17:37:57 -0500

    Triangulo de Floyd

---
+++ triaungulodefloyd.py
@@ -0,0 +1,32 @@
+numero = "1"
+pos = 10
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("1", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("01", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("101", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("0101", end =" ")
+    conteo = conteo +1
+print(" ")
+conteo=0
+while conteo <1:
+    #print(conteo)
+    print ("10101", end =" ")
+    conteo = conteo +1
+print(" ")
